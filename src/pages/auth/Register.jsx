346a8782cf81647cfe103639ceeffa1905c2ed54
Quynh Huong <quynhhuongks2901@gmail.com>
2022-10-06 00:46:25 +0700

    [register-form] fix handleSubmit

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -4,33 +4,36 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import LayoutAuthentication from "../../layout/LayoutAuthentication";
 import RegisterForm from "../../modules/auth/RegisterForm";
 import { Link } from "react-router-dom";
-import './style.css';
+import "./style.css";
 import { Button } from "@chakra-ui/react";
 import { GoogleIcon } from "../../components/icons";
 import * as yup from "yup";
 
 const schema = yup.object({
   fullname: yup.string().required("Please provide a full name."),
-  email: yup.string()
+  email: yup
+    .string()
     .email("Please enter a valid email address.")
     .required("Please provide an email address."),
-  password: yup.string()
+  password: yup
+    .string()
     .required("Please provide a password.")
     .min(8, "Your password must be at least 8 characters long."),
-  confirmPassword: yup.string()
-    .required("Please provide a confirm password.")
-    .oneOf([yup.ref("password")], "Confirm Password must match with Password."),
+  // confirmPassword: yup
+  //   .string()
+  //   .required("Please provide a confirm password.")
+  //   .oneOf([yup.ref("password")], "Confirm Password must match with Password."),
 });
 
-function Register()  {
+function Register() {
   const methods = useForm({
     resolver: yupResolver(schema),
     defaultValues: {
-      fullname: '',
-      email: '',
-      password: '',
+      fullname: "",
+      email: "",
+      password: "",
     },
-    mode: 'onSubmit',
+    mode: "onSubmit",
   });
 
   const onSubmit = (data) => console.log(data);
@@ -41,20 +44,29 @@ function Register()  {
         <meta charSet="utf-8" />
         <title>Register</title>
       </Helmet>
-      <p className="text-register">Already have an account? <Link to='/sign-in' className="text-register-link">Sign in</Link></p>
-      <Button variant='outline' className="btn-google" mb={5}>
+      <p className="text-register">
+        Already have an account?{" "}
+        <Link to="/sign-in" className="text-register-link">
+          Sign in
+        </Link>
+      </p>
+      <Button variant="outline" className="btn-google" mb={5}>
         <GoogleIcon />
         <span>Sign up with google</span>
       </Button>
-      <Button className="link-email" variant='link'>Or sign up with email</Button>
+      <Button className="link-email" variant="link">
+        Or sign up with email
+      </Button>
       <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(onSubmit)}>
           <RegisterForm />
-          <Button type="submit" className="btn-submit" colorScheme='purple' >Create my account</Button>
+          <Button type="submit" className="btn-submit" colorScheme="purple">
+            Create my account
+          </Button>
         </form>
       </FormProvider>
     </LayoutAuthentication>
   );
-};
+}
 
 export default Register;
