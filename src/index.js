f43dc97567a22e9110505885b5102892892b57d2
Hai Yen Nguyen <nguyenthihaiyen120397@gmail.com>
2022-11-23 17:00:35 +0700

    feat/addSentry

--- src/index.js
+++ src/index.js
@@ -8,6 +8,23 @@ import { Provider } from 'react-redux';
 import store from "./store/store";
 import 'react-toastify/dist/ReactToastify.css';
 import { theme } from "./theme/theme";
+//Add Sentry
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+
+
+
+//Add Sentry
+Sentry.init({
+  dsn: "https://d396429f27a149e19d80966bd8db28be@o4504185311002624.ingest.sentry.io/4504200551596032",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
