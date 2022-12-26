08e682a821035d31522e0eaa985555b33b91aa1e
Quynh Huong <quynhhuongks2901@gmail.com>
2022-12-27 03:13:06 +0700

    feat: create a review

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,24 +1,26 @@
-export { default as UserIcon } from './cases/UserIcon';
-export { default as CartIcon } from './cases/CartIcon';
-export { default as LogoIcon } from './cases/LogoIcon';
-export { default as GoogleIcon } from './cases/GoogleIcon';
+export { default as UserIcon } from "./cases/UserIcon";
+export { default as CartIcon } from "./cases/CartIcon";
+export { default as LogoIcon } from "./cases/LogoIcon";
+export { default as GoogleIcon } from "./cases/GoogleIcon";
 
-export { default as TimeIcon } from './cases/TimeIcon';
-export { default as PayIcon } from './cases/PayIcon';
-export { default as QualityIcon } from './cases/QualityIcon';
-export { default as GuardIcon } from './cases/GuardIcon';
+export { default as TimeIcon } from "./cases/TimeIcon";
+export { default as PayIcon } from "./cases/PayIcon";
+export { default as QualityIcon } from "./cases/QualityIcon";
+export { default as GuardIcon } from "./cases/GuardIcon";
 
-export { default as StarIcon } from './cases/StarIcon'
-export { default as ArrowRightIcon } from './cases/ArrowRightIcon';
-export { default as PrevRightIcon } from './cases/PrevRightIcon';
+export { default as StarIcon } from "./cases/StarIcon";
+export { default as ArrowRightIcon } from "./cases/ArrowRightIcon";
+export { default as PrevRightIcon } from "./cases/PrevRightIcon";
 
-export { default as BookIcon } from './cases/BookIcon';
-export { default as CustomerIcon } from './cases/CustomerIcon';
+export { default as BookIcon } from "./cases/BookIcon";
+export { default as CustomerIcon } from "./cases/CustomerIcon";
 
-export { default as CircleElement } from './sliderbanner/CircleElement';
+export { default as CircleElement } from "./sliderbanner/CircleElement";
 
-export { default as ArrowDownIcon } from './cases/ArrowDownIcon';
-export { default as ArrowUpIcon } from './cases/ArrowUpIcon';
+export { default as ArrowDownIcon } from "./cases/ArrowDownIcon";
+export { default as ArrowUpIcon } from "./cases/ArrowUpIcon";
 
-export { default as AddIcon } from './cases/AddIcon';
-export { default as SubIcon } from './cases/SubIcon';
+export { default as AddIcon } from "./cases/AddIcon";
+export { default as SubIcon } from "./cases/SubIcon";
+
+export { default as AvatarIcon } from "./cases/AvatarIcon";
